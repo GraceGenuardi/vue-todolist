b2b068e30a051ef39ddd301cc506f2465464b32e
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-21 16:15:59 +0100

    EDIT completato Milstone 3

--- js/app.js
+++ js/app.js
@@ -1,17 +1,9 @@
-//MILESTONE 1
-  //Stampare all’interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-
-
-//MILESTONE 2
-  //Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
-
-
-
-//MILESTONE 3
-  //Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 
   const { createApp } = Vue;
+
+//MILESTONE 1
+  //Stampare all’interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
   createApp({
   data() {
@@ -21,8 +13,35 @@
   { text: "Meeting with the SEO specialist", done: false },
   { text: "Contact Yummy-tech customer for website restyling", done: true },
   { text: "Afternoon Team building event", done: false },
+  { text: "Completed the safety at work form", done: true },
   ],
   newTaskText: "",
   };
-  }
+  },
+
+ 
+
+  methods: {
+
+    //MILESTONE 3
+    //Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+     
+    addNewTask() {
+    if (this.newTaskText !== "") {
+    this.tasks.push({
+    text: this.newTaskText,
+    done: false,
+    });
+    this.newTaskText = "";
+    }
+    },
+
+ //MILESTONE 2
+  //Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.   
+    removeTask(index) {
+    this.tasks.splice(index, 1);
+    },
+    },
+
+  
   }).mount("#app"); 
